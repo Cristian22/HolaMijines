e66c25e3b10b3c890267bc877ae89cfda4d911e6
Cristian22 <cristianmacasn@gmail.com>
2015-03-14 12:03:57 -0500

    Se agrego autores del proyecto

--- src/HolaMijines.java
+++ src/HolaMijines.java
@@ -1,6 +1,9 @@
 
 import javax.swing.JOptionPane;
-
+/**
+*
+* Cristian y Alex
+*/
 public class HolaMijines {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Bienvenido a GitHub");
